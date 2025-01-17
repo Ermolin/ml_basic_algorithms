07c80bb7e4ecb367262a7229aef280db32e1831e
Aleksandr Ermolin <aa1ermolin@gmail.com>
2025-01-17 12:33:16 +0300

    add more metrics and method to get final score

--- MyLineReg.py
+++ MyLineReg.py
@@ -7,25 +7,75 @@ class MyLineReg():
             self,
             n_iter: int = 10,
             learning_rate: float = 0.5,
-            weights: np.array = None
+            metric: str = None,
+            weights: np.array = None,
+            metric_value: float = None
     ):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
+        self.metric = metric
+        self.metric_value = metric_value
 
     def __str__(self):
         return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
     
     def MSE(
             self,
-            y: pd.Series, # Series with real values
-            y_cap : pd.Series, # Series with predicted values
+            y: np.array, # Series with real values
+            y_cap : np.array, # Series with predicted values
             ):
         '''
         calculate MSE
         '''
         errors = y-y_cap
-        mse = sum([float(i)**2 for i in errors])/len(errors)
-        return mse
+        return sum([float(i)**2 for i in errors])/len(errors)
+         
+    def MAE(
+            self,
+            y: np.array, # Series with real values
+            y_cap : np.array, # Series with predicted values
+            ):
+        '''
+        calculate MAE
+        '''
+        errors = y-y_cap
+        return sum([np.abs(i) for i in errors])/len(errors)
+
+    def RMSE(
+            self,
+            y: np.array, # Series with real values
+            y_cap : np.array, # Series with predicted values
+            ):
+        '''
+        calculate RMSE
+        '''
+        errors = y-y_cap
+        return (sum([float(i)**2 for i in errors])/len(errors))** (1/2)
+    
+    def R2(
+            self,
+            y: np.array, # Series with real values
+            y_cap : np.array, # Series with predicted values
+            ):
+        '''
+        calculate R^2
+        '''
+        errors = y-y_cap
+        y_mean = np.mean(y)
+        errors2 = y - y_mean
+        return 1 - (sum([float(i)**2 for i in errors]) / sum([float(i)**2 for i in errors2]))
+
+    def MAPE(
+            self,
+            y: np.array, # Series with real values
+            y_cap : np.array, # Series with predicted values
+            ):
+        '''
+        calculate MAPE
+        '''
+        errors = y-y_cap
+        part_errors = np.abs(errors / y)
+        return 100 * sum(part_errors)/len(y)
 
     def ANTIGRADIENT(
             self,
@@ -40,6 +90,32 @@ class MyLineReg():
         errors_df = errors * X.transpose()
         antigradient = - (errors_df.sum(axis=1) / X.shape[0] )*2
         return antigradient
+
+    def calc_error(
+            self,
+            y: pd.Series, # Series with real values
+            y_cap : pd.Series, # Series with predicted values
+            ):
+        '''
+        calculate selected metric
+        '''
+        if self.metric == 'mse':
+            return self.MSE(y=y,
+                            y_cap=y_cap)
+        elif self.metric == 'mae':
+            return self.MAE(y=y,
+                            y_cap=y_cap)
+        elif self.metric == 'rmse':
+            return self.RMSE(y=y,
+                            y_cap=y_cap)
+        elif self.metric == 'r2':
+            return self.R2(y=y,
+                            y_cap=y_cap)
+        elif self.metric == 'mape':
+            return self.MAPE(y=y,
+                            y_cap=y_cap)
+        else:
+            return None
 
     def fit(
             self,
@@ -74,12 +150,19 @@ class MyLineReg():
             W = W + (antigradient* self.learning_rate)
             self.weights = W
 
-            if verbose:
+            self.metric_value = self.calc_error(y=y,y_cap=y_cap)
+
+            if verbose and self.metric :
                 if i == 0:
-                    print(f'start | loss: {mse}') 
+                    print(f'start | loss: {mse} | {self.metric} : {self.metric_value}') 
                 
                 elif (i+1) % verbose == 0:
-                    print(f'{i} | loss: {mse}')
+                    print(f'{i} | loss: {mse} | {self.metric} : {self.metric_value}') 
+        
+        # update metric after all changes
+        X_vals =X*W
+        y_cap = X_vals.sum(axis=1)
+        self.metric_value = self.calc_error(y=y,y_cap=y_cap)
 
     def get_coef(self):
         '''
@@ -97,3 +180,8 @@ class MyLineReg():
         X=X[['x0']+feats]
         X=np.array(X)
         return pd.Series((X*W).sum(axis=1))
+    
+    def get_best_score(
+                       self
+                      ):
+        return self.metric_value
