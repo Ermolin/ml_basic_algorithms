4837f6e28b1881d57dbb0f53c4a1ec8ca49ea47c
Aleksandr Ermolin <aa1ermolin@gmail.com>
2025-01-23 18:17:56 +0300

    knnclf init

---
+++ knnclf.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+
+class MyKNNClf():
+    def __init__(
+            self,
+            k: int = 3,
+    ):
+        self.k = k
+
+
+    def __str__(self):
+        return f'MyKNNClf class: k={self.k}'
+
+
+
